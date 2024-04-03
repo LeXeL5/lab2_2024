2f843fcf69cfeadd025d9b70c74a65ce32dcc388
LeXeL5 <lexel_2001@bk.ru>
2024-04-03 22:53:18 +0300

    versin 1.0

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 using namespace std;
-
 struct node {
     int value = 0;
     node* next_node = nullptr;
 };
-
 struct stack {
     node* head = nullptr;
     node* tail = nullptr;
     int counter = 0;
-
     void queue(int value) {
         node* new_node = new node();
         new_node->value = value;
@@ -24,7 +21,6 @@ struct stack {
         }
         counter++;
     }
-
     int unqueue() {
         node* new_head = head->next_node;
         int value = head->value;
@@ -33,54 +29,55 @@ struct stack {
         counter--;
         return value;
     }
-
     int count() {
         return counter;
     }
-
     void insertBeforeNegative(){
         node* current_node = head;
-        while (current_node->next_node != nullptr) {
-            if (current_node->next_node->value < 0) {
+        if (counter > 0) {
+            while (current_node->next_node != nullptr) {
+                if (current_node->next_node->value < 0) {
+                    node* new_node = new node();
+                    new_node->value = 1;
+                    new_node->next_node = current_node->next_node;
+                    current_node->next_node = new_node;
+                    current_node = current_node->next_node;
+                    counter++;
+                }
+                current_node = current_node->next_node;
+            }
+            if (head->value < 0) {
                 node* new_node = new node();
                 new_node->value = 1;
-                new_node->next_node = current_node->next_node;
-                current_node->next_node = new_node;
-                current_node = current_node->next_node;
+                new_node->next_node = head;
+                head = new_node;
                 counter++;
             }
-            current_node = current_node->next_node;
-        }
-        if (head->value < 0) {
-            node* new_node = new node();
-            current_node = head;
-            new_node->value = 1;
-            new_node->next_node = current_node->next_node;
-            head = new_node;
-            delete current_node;
         }
     }
-
     void removeNegative(){
-        node* current_node = head;
-        node* ptr = nullptr;
-        while (current_node->next_node != nullptr) {
-            if (current_node->next_node->value < 0) {
-                ptr = current_node->next_node->next_node;
-                delete current_node->next_node;
-                current_node->next_node = ptr;
+        if (counter > 0) {
+            node* current_node = head;
+            node* ptr = nullptr;
+            while (current_node->next_node != nullptr) {
+                if (current_node->next_node->value < 0) {
+                    ptr = current_node->next_node->next_node;
+                    delete current_node->next_node;
+                    current_node->next_node = ptr;
+                    counter--;
+                }
+                else {
+                    current_node = current_node->next_node;
+                }
             }
-            else {
-                current_node = current_node->next_node;
+            if (head->value < 0) {
+                current_node = head;
+                head = head->next_node;
+                delete current_node;
+                counter--;
             }
-        }
-        if (head->value < 0) {
-            current_node = head;
-            head = head->next_node;
-            delete current_node;
         }
     }
-
     int count(int value) {
         int count_value = 0;
         node* current_node = head;
@@ -92,14 +89,12 @@ struct stack {
         }
         return count_value;
     }
-
     void clear(){
         int cize = count();
         for (int i = 0; i < cize; i++) {
             unqueue();
         }
     }
-
     void menu() {
         cout << "(0) shutdown program" << endl;
         cout << "(1) queue(int value)" << endl;
@@ -111,7 +106,6 @@ struct stack {
         cout << "(7) clear()" << endl;
     }
 };
-
 void main(int argc, char* argv[]) {
     bool interface = true;
     string argument;
@@ -119,33 +113,78 @@ void main(int argc, char* argv[]) {
     if (argument == "false" || argument == "0") { interface = false; }
     stack list;
     int value;
+    int count_what = 0;
     if (interface) {
-        cout << "Enter sequence of numbers (sequence end sign 0):" << endl;
-    }
-    while (true) {
-        cin >> value;
-        if (value != 0) {
-            list.queue(value);
+        int switch_on = 1;
+        while (switch_on) {
+            system("cls");
+            list.menu();
+            cin >> switch_on;
+            switch (switch_on) {
+            case (0): {
+                break;
+                }      
+            case (1): {
+                while (true) {
+                    cin >> value;
+                    if (value != 0) {
+                        list.queue(value);
+                    }
+                    else {
+                        break;
+                    }
+                }
+                break;
+                }  
+            case (2): {
+                if (list.count() > 0) {
+                    cout << list.unqueue() << endl;
+                }
+                break;
+                }     
+            case (3): {
+                cout << list.count() << endl;
+                break;
+                }    
+            case (4): {
+                list.insertBeforeNegative();
+                break;
+                }  
+            case (5): {
+                list.removeNegative();
+                break;
+                }  
+            case (6): {
+                cin >> count_what;
+                cout << list.count(count_what) << endl;
+                break;
+                }   
+            case (7): {
+                list.clear();
+                break;
+                }  
+            default: {
+                cout << "unknown command, try again" << endl;
+                break;
+                }
+            }
+            system("pause");
         }
-        else {
-            break;
+    }
+    else {
+        while (true) {
+            cin >> value;
+            if (value != 0) {
+                list.queue(value);
+            }
+            else {
+                break;
+            }
         }
-    }
-
-    list.insertBeforeNegative();
-    list.removeNegative();
-    cout << endl << list.count(2) << endl;
-
-    if (interface) {
-        cout << "New list:" << endl;
-    }
-    int size = list.count();
-    for (int i = 0; i < size; i++) {
-        value = list.unqueue();
-        cout << value << endl;
-    }
-
-    if (interface) {
-        system("pause");
+        list.insertBeforeNegative();
+        list.removeNegative();
+        cin >> count_what;
+        cout << list.count(count_what) << endl;
+        list.clear();
     }
 }
